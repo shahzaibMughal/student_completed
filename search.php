19d1ace0b6859888c2ca31b2c200b2df3723db37
Zaheer <zaheer.mughal1965@gmail.com>
2019-02-11 21:27:56 +0500

    completed

--- search.php
+++ search.php
@@ -32,33 +32,44 @@ $db = mysqli_connect("localhost","root",'','ss');
       .nav-link:hover{
         color: #eee;
       }
+
+      .table-responsive{
+        width: 100%;
+      }
       form{
-        width: 20%;
+        width: 30%;
         margin: 0 auto;
       }
 
     </style>
   </head>
   <body>
-<nav>
-  <ul class="nav ">
-    <li class="nav-item navbar-brand "> Student Management System</li>
-    <li  class="nav-item"> <a class="nav-link active" href="user_registration_new.php">Add Record</a></li>
-    <li class="nav-item"> <a class="nav-link"  href="update_students.php">Update Record</a> </li>
-    <li class="nav-item"> <a class="nav-link"  href="delete_students.php">Delete Record</a> </li>
-    <li class="nav-item"> <a class="nav-link"  href="view_students.php">Show Record</a> </li>
-    <li class="nav-item"> <a class="nav-link"  href="search.php">Search Record</a> </li>
-  </ul>
-</nav>
-<form action='search.php' method='get' class="">
-  <div class='search_record' > <h2 class="text-center"> Search A Record </h2>
-  <div class="input-group">
-    <input class=" form-control" type='text' name ='search' placeholder="Enter CNIC TO SEARCH" required>
-    <input type='submit' class="btn btn-primary" >
-  </div>
-</form>
+    <nav>
+      <ul class="nav ">
+        <li class="nav-item navbar-brand "> Student Management System</li>
+        <li  class="nav-item"> <a class="nav-link active" href="user_registration_new.php">Add Record</a></li>
+        <li class="nav-item"> <a class="nav-link"  href="update_students.php">Update Record</a> </li>
+        <li class="nav-item"> <a class="nav-link"  href="delete_students.php">Delete Record</a> </li>
+        <li class="nav-item"> <a class="nav-link"  href="view_students.php">Show Record</a> </li>
+        <li class="nav-item"> <a class="nav-link"  href="search.php">Search Record</a> </li>
+      </ul>
+    </nav>
+    <div class="jumbotron ">
+      <form  class="search_form" action='search.php' method='get' class="">
+        <div class='search_record' >
+          <h2 class="text-center"> Search A Record </h2>
+          <div class="input-group">
+            <input class=" form-control" type='text' name ='search' placeholder="Enter CNIC TO SEARCH" required>
+            <input type='submit' class="btn btn-primary" >
+          </div>
+        </div>
+        </form>
+    </div>
+
+
 
 <?php
+
 if(isset($_GET['search']))
 {
   // select * from u_reg where cnic LIKE '34%'
@@ -79,21 +90,44 @@ if(isset($_GET['search']))
       $mobile1=$row2['mobile'];
 ?>
 
-      <table>
-        <tr>
-          <td><?php echo $cnic1; ?></td>
-          <td><?php echo $name1; ?></td>
-          <td><?php echo $dob1; ?></td>
-          <td><?php echo $address1; ?></td>
-          <td><?php echo $city1; ?></td>
-          <td><?php echo $degree1; ?></td>
-          <td><?php echo $gender1; ?></td>
-          <td><?php echo $email1; ?></td>
-          <td><?php echo $mobile1; ?></td>
-        </tr>
+
+
+      <div class="table-responsive">
+        <table class="table table-striped" >
+          <thead>
+            <tr>
+              <th scope="col">CNIC</th>
+              <th  scope="col" >Name</th>
+              <th  scope="col" >DOB</th>
+              <th  scope="col" >Address</th>
+              <th  scope="col" >City</th>
+              <th  scope="col" >Degree Program</th>
+              <th  scope="col" >Gender</th>
+              <th  scope="col" >Email</th>
+              <th  scope="col" >Mobile</th>
+              <th  scope="col" ></th>
+              <th  scope="col" ></th>
+            </tr>
+          </thead>
+          <tbody>
+
+            <?php  while($student = mysqli_fetch_assoc($resultSet)){?>
+              <tr>
+                <td scope="row" ><?php echo $cnic1; ?></td>
+                <td><?php echo $name1; ?></td>
+                <td><?php echo $dob1; ?></td>
+                <td><?php echo $address1; ?></td>
+                <td><?php echo $city1; ?></td>
+                <td><?php echo $degree1; ?></td>
+                <td><?php echo $gender1; ?></td>
+                <td><?php echo $email1; ?></td>
+                <td><?php echo $mobile1; ?></td>
+              </tr>
+            <?php }?>
+          </tbody>
+        </table>
+      </div>
 
       <?php }} ?>
-</table>
-</div>
 </body>
 </html>
